e1b49b224eab2b489f0ea8bde2cb5919300ec01d
Cesar Murillo <namiayumis@gmail.com>
2019-09-02 12:18:36 +0900

    begin project

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,18 @@
+import 'package:design/src/pages/basico_page.dart';
+import 'package:flutter/material.dart';
 
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Diseños',
+      initialRoute: 'basico',
+      routes: {
+        'basico': (BuildContext context) => BasicoPage(),
+      },
+    );
+  }
+}
