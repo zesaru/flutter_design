0e7da57c0d63ac093f3b2aae0ae2fd896bae6004
Cesar Murillo <namiayumis@gmail.com>
2019-09-02 15:24:59 +0900

    design part1

--- lib/src/pages/basico_page.dart
+++ lib/src/pages/basico_page.dart
@@ -1,11 +1,41 @@
 import 'package:flutter/material.dart';
 
 class BasicoPage extends StatelessWidget {
+  final estiloTitulo = TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold);
+  final estiloSubtitulo = TextStyle(fontSize: 18.0, color: Colors.grey);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Basico Page'),
+      body: Column(
+        children: <Widget>[
+          Image(
+            image: NetworkImage(
+                'https://images.pexels.com/photos/414171/pexels-photo-414171.jpeg?cs=srgb&dl=adventure-calm-clouds-414171.jpg&fm=jpg'),
+          ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text('Lago en las montañas', style: estiloTitulo),
+                    SizedBox(height: 7.0),
+                    Text('Hermoso paisaje', style: estiloSubtitulo)
+                  ],
+                ),
+              ),
+              Icon(
+                Icons.star,
+                color: Colors.red,
+              ),
+              Text(
+                '41',
+                style: TextStyle(fontSize: 20),
+              )
+            ],
+          )
+        ],
       ),
     );
   }
