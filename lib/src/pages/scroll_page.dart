7a3542bb33d5df6f4bbb3c767f621f1cc84cd146
Cesar Murillo <namiayumis@gmail.com>
2019-09-04 11:15:14 +0900

    scroll part 2

--- lib/src/pages/scroll_page.dart
+++ lib/src/pages/scroll_page.dart
@@ -15,9 +15,45 @@ class ScrollPage extends StatelessWidget {
   }
 
   Widget _pagina1() {
-    return Center(
-      child: Center(
-        child: Text('Page 1'),
+    return Stack(
+      children: <Widget>[
+        _colorFondo(),
+        _imagenFondo(),
+        _textos(),
+      ],
+    );
+  }
+
+  Widget _colorFondo() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Color.fromRGBO(108, 192, 218, 1.0),
+    );
+  }
+
+  Widget _imagenFondo() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      child: Image(
+        image: AssetImage('assets/scroll-1.png'),
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+
+  Widget _textos() {
+    final _estiloTexto = TextStyle(color: Colors.white, fontSize: 50.0);
+    return SafeArea(
+      child: Column(
+        children: <Widget>[
+          SizedBox(height: 20.0),
+          Text('11º', style: _estiloTexto),
+          Text('Miercoles', style: _estiloTexto),
+          Expanded(child: Container()),
+          Icon(Icons.keyboard_arrow_down, color: Colors.white, size: 70.0)
+        ],
       ),
     );
   }
