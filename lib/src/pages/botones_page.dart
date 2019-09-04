61d1c2e683f130451c59d0c7a0f688ea3f4dfec6
Cesar Murillo <namiayumis@gmail.com>
2019-09-04 22:06:58 +0900

    diseno compuesto Table y Row

--- lib/src/pages/botones_page.dart
+++ lib/src/pages/botones_page.dart
@@ -9,7 +9,14 @@ class BotonesPage extends StatelessWidget {
         body: Stack(
           children: <Widget>[
             _fondoApp(),
-            _titulos(),
+            SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  _titulos(),
+                  _botonesRedondeados(),
+                ],
+              ),
+            )
           ],
         ),
         bottomNavigationBar: _bottomNavigationBar(context));
@@ -97,6 +104,60 @@ class BotonesPage extends StatelessWidget {
           BottomNavigationBarItem(
               icon: Icon(Icons.supervised_user_circle, size: 30.0),
               title: Container()),
+        ],
+      ),
+    );
+  }
+
+  Widget _botonesRedondeados() {
+    return Table(
+      children: [
+        TableRow(children: [
+          _crearBotonRedondeado(),
+          _crearBotonRedondeado(),
+        ]),
+        TableRow(children: [
+          _crearBotonRedondeado(),
+          _crearBotonRedondeado(),
+        ]),
+        TableRow(children: [
+          _crearBotonRedondeado(),
+          _crearBotonRedondeado(),
+        ]),
+        TableRow(children: [
+          _crearBotonRedondeado(),
+          _crearBotonRedondeado(),
+        ])
+      ],
+    );
+  }
+
+  Widget _crearBotonRedondeado() {
+    return Container(
+      height: 180.0,
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: Color.fromRGBO(62, 66, 107, 0.7),
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          SizedBox(height: 5.0),
+          CircleAvatar(
+            backgroundColor: Colors.pinkAccent,
+            radius: 35.0,
+            child: Icon(
+              Icons.swap_calls,
+              color: Colors.white,
+              size: 30.0,
+            ),
+          ),
+          Text(
+            'data',
+            style: TextStyle(color: Colors.pinkAccent),
+          ),
+          SizedBox(height: 5.0)
         ],
       ),
     );
