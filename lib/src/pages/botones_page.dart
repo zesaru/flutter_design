982e2bea846f7b3905fe3217946482be4b2413d3
Cesar Murillo <namiayumis@gmail.com>
2019-09-04 22:09:26 +0900

    diseno blur y argumentos

--- lib/src/pages/botones_page.dart
+++ lib/src/pages/botones_page.dart
@@ -1,6 +1,7 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
+
+import 'dart:math';
+import 'dart:ui';
 
 class BotonesPage extends StatelessWidget {
   @override
@@ -11,10 +12,7 @@ class BotonesPage extends StatelessWidget {
             _fondoApp(),
             SingleChildScrollView(
               child: Column(
-                children: <Widget>[
-                  _titulos(),
-                  _botonesRedondeados(),
-                ],
+                children: <Widget>[_titulos(), _botonesRedondeados()],
               ),
             )
           ],
@@ -27,37 +25,30 @@ class BotonesPage extends StatelessWidget {
       width: double.infinity,
       height: double.infinity,
       decoration: BoxDecoration(
-        gradient: LinearGradient(
-            begin: FractionalOffset(0.0, 0.6),
-            end: FractionalOffset(0.0, 1.0),
-            colors: [
-              Color.fromRGBO(52, 54, 101, 1.0),
-              Color.fromRGBO(35, 37, 57, 1.0),
-            ]),
-      ),
+          gradient: LinearGradient(
+              begin: FractionalOffset(0.0, 0.6),
+              end: FractionalOffset(0.0, 1.0),
+              colors: [
+            Color.fromRGBO(52, 54, 101, 1.0),
+            Color.fromRGBO(35, 37, 57, 1.0)
+          ])),
     );
 
     final cajaRosa = Transform.rotate(
-      angle: -pi / 5.0,
-      child: Container(
+        angle: -pi / 5.0,
+        child: Container(
           height: 360.0,
           width: 360.0,
           decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(90.0),
-            gradient: LinearGradient(
-              colors: [
+              borderRadius: BorderRadius.circular(80.0),
+              gradient: LinearGradient(colors: [
                 Color.fromRGBO(236, 98, 188, 1.0),
-                Color.fromRGBO(241, 142, 172, 1.0),
-              ],
-            ),
-          )),
-    );
+                Color.fromRGBO(241, 142, 172, 1.0)
+              ])),
+        ));
 
     return Stack(
-      children: <Widget>[
-        gradiente,
-        Positioned(top: -100, child: cajaRosa),
-      ],
+      children: <Widget>[gradiente, Positioned(top: -100.0, child: cajaRosa)],
     );
   }
 
@@ -68,20 +59,14 @@ class BotonesPage extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Text(
-              'Clasify transaction',
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 30.0,
-                  fontWeight: FontWeight.bold),
-            ),
-            Text(
-              'Classify this transaction into a particular category',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 18.0,
-              ),
-            ),
+            Text('Classify transaction',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold)),
+            SizedBox(height: 10.0),
+            Text('Classify this transaction into a particular category',
+                style: TextStyle(color: Colors.white, fontSize: 18.0)),
           ],
         ),
       ),
@@ -113,52 +98,51 @@ class BotonesPage extends StatelessWidget {
     return Table(
       children: [
         TableRow(children: [
-          _crearBotonRedondeado(),
-          _crearBotonRedondeado(),
+          _crearBotonRedondeado(Colors.blue, Icons.border_all, 'General'),
+          _crearBotonRedondeado(
+              Colors.purpleAccent, Icons.directions_bus, 'Bus'),
         ]),
         TableRow(children: [
-          _crearBotonRedondeado(),
-          _crearBotonRedondeado(),
+          _crearBotonRedondeado(Colors.pinkAccent, Icons.shop, 'Buy'),
+          _crearBotonRedondeado(Colors.orange, Icons.insert_drive_file, 'File'),
         ]),
         TableRow(children: [
-          _crearBotonRedondeado(),
-          _crearBotonRedondeado(),
+          _crearBotonRedondeado(
+              Colors.blueAccent, Icons.movie_filter, 'Entertaiment'),
+          _crearBotonRedondeado(Colors.green, Icons.cloud, 'Grocery'),
         ]),
         TableRow(children: [
-          _crearBotonRedondeado(),
-          _crearBotonRedondeado(),
+          _crearBotonRedondeado(Colors.red, Icons.collections, 'Photos'),
+          _crearBotonRedondeado(Colors.teal, Icons.help_outline, 'General'),
         ])
       ],
     );
   }
 
-  Widget _crearBotonRedondeado() {
-    return Container(
-      height: 180.0,
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        color: Color.fromRGBO(62, 66, 107, 0.7),
-        borderRadius: BorderRadius.circular(20.0),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
-          SizedBox(height: 5.0),
-          CircleAvatar(
-            backgroundColor: Colors.pinkAccent,
-            radius: 35.0,
-            child: Icon(
-              Icons.swap_calls,
-              color: Colors.white,
-              size: 30.0,
-            ),
+  Widget _crearBotonRedondeado(Color color, IconData icono, String texto) {
+    return ClipRect(
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+        child: Container(
+          height: 180.0,
+          margin: EdgeInsets.all(15.0),
+          decoration: BoxDecoration(
+              color: Color.fromRGBO(62, 66, 107, 0.7),
+              borderRadius: BorderRadius.circular(20.0)),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              SizedBox(height: 5.0),
+              CircleAvatar(
+                backgroundColor: color,
+                radius: 35.0,
+                child: Icon(icono, color: Colors.white, size: 30.0),
+              ),
+              Text(texto, style: TextStyle(color: color)),
+              SizedBox(height: 5.0)
+            ],
           ),
-          Text(
-            'data',
-            style: TextStyle(color: Colors.pinkAccent),
-          ),
-          SizedBox(height: 5.0)
-        ],
+        ),
       ),
     );
   }
